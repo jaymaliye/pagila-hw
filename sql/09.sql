ec8bfb4a8c4bdd07c6781be003c9cc07e007908d
Jay Maliye <jaymaliye@gmail.com>
2023-02-20 14:47:23 -0800

    fourth

--- sql/09.sql
+++ sql/09.sql
@@ -6,8 +6,8 @@
 SELECT first_name, 
        last_name,
        sum(amount) AS sum
-FROM staff
-LEFT JOIN payment
-    ON staff.staff_id = payment.staff_id
+FROM payment
+INNER JOIN staff
+    ON payment.staff_id = staff.staff_id
 WHERE payment_date BETWEEN '2019-12-31' AND '2020-02-01'
 GROUP BY staff.staff_id;
